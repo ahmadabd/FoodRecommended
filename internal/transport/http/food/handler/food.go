3ed5271f9f61ba19716d39baa7d850e7a799c4ea
alux <ahmadabd13741112@gmail.com>
2022-02-18 11:00:34 +0330

    better design for handlers, now we can pass parameter to handler methods

--- internal/transport/http/food/handler/food.go
+++ internal/transport/http/food/handler/food.go
@@ -18,79 +18,85 @@ type handler struct {
 	logger logger.Logger
 }
 
-func (handler *handler) randomFoodHandler(c echo.Context) error {
+func (handler *handler) randomFoodHandler() echo.HandlerFunc {
 
-	food, err := handler.food.RandomFood(c.Request().Context())
+	return func(c echo.Context) error {
+		food, err := handler.food.RandomFood(c.Request().Context())
 
-	if err != nil {
-		handler.logger.Error(err.Error())
+		if err != nil {
+			handler.logger.Error(err.Error())
 
-		c.JSON(http.StatusInternalServerError, response.Error{
-			Status:  http.StatusInternalServerError,
-			Message: err.Error(),
-		})
-	}
-
-	return c.JSON(http.StatusOK, castFoodToResponse(food))
-}
-
-func (handler *handler) addFoodHandler(c echo.Context) error {
-
-	newFood := new(request.Food)
-
-	if err := c.Bind(newFood); err != nil {
-		handler.logger.Error(err.Error())
-
-		c.JSON(http.StatusBadRequest, response.Error{
-			Status:  http.StatusBadRequest,
-			Message: err.Error(),
-		})
-	}
-
-	food := castRequestToFood(*newFood)
-
-	if err := handler.food.StoreFood(c.Request().Context(), *food); err != nil {
-		handler.logger.Error(err.Error())
-		c.JSON(http.StatusInternalServerError, response.Error{
-			Status:  http.StatusInternalServerError,
-			Message: err.Error(),
-		})
-	}
-
-	return c.JSON(http.StatusCreated, response.Error{
-		Status:  http.StatusCreated,
-		Message: "Success",
-	})
-}
-
-func (handler *handler) allFoodsHandler(c echo.Context) error {
-
-	paginationLimit, perr := strconv.ParseInt(c.QueryParam("paginationLimit"), 10, 64)
-	if perr != nil || paginationLimit == 0 {
-		paginationLimit = 10
-	}
-
-	var foods []model.Food
-	var err error
-	foods, err = handler.food.GetFoods(c.Request().Context(), int(paginationLimit))
-	if err != nil {
-		handler.logger.Error(err.Error())
-		c.JSON(http.StatusInternalServerError, response.Error{
-			Status:  http.StatusInternalServerError,
-			Message: err.Error(),
-		})
-	}
-
-	res := new([]response.Food)
-
-	if err == nil {
-		for _, food := range foods {
-			tmp := castFoodToResponse(food)
-			*res = append(*res, *tmp)
+			c.JSON(http.StatusInternalServerError, response.Error{
+				Status:  http.StatusInternalServerError,
+				Message: err.Error(),
+			})
 		}
-	}
 
-	return c.JSON(http.StatusOK, res)
+		return c.JSON(http.StatusOK, castFoodToResponse(food))
+	}
+}
+
+func (handler *handler) addFoodHandler() echo.HandlerFunc {
+
+	return func(c echo.Context) error {
+		newFood := new(request.Food)
+
+		if err := c.Bind(newFood); err != nil {
+			handler.logger.Error(err.Error())
+
+			c.JSON(http.StatusBadRequest, response.Error{
+				Status:  http.StatusBadRequest,
+				Message: err.Error(),
+			})
+		}
+
+		food := castRequestToFood(*newFood)
+
+		if err := handler.food.StoreFood(c.Request().Context(), *food); err != nil {
+			handler.logger.Error(err.Error())
+			c.JSON(http.StatusInternalServerError, response.Error{
+				Status:  http.StatusInternalServerError,
+				Message: err.Error(),
+			})
+		}
+
+		return c.JSON(http.StatusCreated, response.Error{
+			Status:  http.StatusCreated,
+			Message: "Success",
+		})
+	}
+}
+
+func (handler *handler) allFoodsHandler() echo.HandlerFunc {
+
+	return func(c echo.Context) error {
+		paginationLimit, perr := strconv.ParseInt(c.QueryParam("paginationLimit"), 10, 64)
+		if perr != nil || paginationLimit == 0 {
+			paginationLimit = 10
+		}
+
+		var foods []model.Food
+		var err error
+		foods, err = handler.food.GetFoods(c.Request().Context(), int(paginationLimit))
+		if err != nil {
+			handler.logger.Error(err.Error())
+			c.JSON(http.StatusInternalServerError, response.Error{
+				Status:  http.StatusInternalServerError,
+				Message: err.Error(),
+			})
+		}
+
+		res := new([]response.Food)
+
+		if err == nil {
+			for _, food := range foods {
+				tmp := castFoodToResponse(food)
+				*res = append(*res, *tmp)
+			}
+		}
+
+		return c.JSON(http.StatusOK, res)
+	}
 }
 
 func castFoodToResponse(food model.Food) *response.Food {
