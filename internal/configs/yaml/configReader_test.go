1bc882c3ed5fe94a53e90199f49b087cc9e8e59a
alux <ahmadabd13741112@gmail.com>
2022-02-25 09:45:11 +0330

    test yaml funcs

--- internal/configs/yaml/configReader_test.go
+++ internal/configs/yaml/configReader_test.go
@@ -6,6 +6,76 @@ import (
 	"github.com/ahmadabd/FoodRecommended.git/internal/configs"
 )
 
+func TestGetServerHost(t *testing.T) {
+	path := "testdata/config.yml"
+
+	cfg, _ := GetConfig(path)
+
+	if cfg.GetServerHost() != "localhost" {
+		t.Errorf("got, want %s", cfg.GetServerHost())
+	}
+}
+
+func TestGetDatabaseHost(t *testing.T) {
+	path := "testdata/config.yml"
+
+	cfg, _ := GetConfig(path)
+
+	if cfg.GetDatabaseHost() != "localhost" {
+		t.Errorf("got, want %s", cfg.GetDatabaseHost())
+	}
+}
+
+func TestGetServerPort(t *testing.T) {
+	path := "testdata/config.yml"
+
+	cfg, _ := GetConfig(path)
+
+	if cfg.GetServerPort() != "5000" {
+		t.Errorf("got, want %s", cfg.GetServerPort())
+	}
+}
+
+func TestGetDatabasePort(t *testing.T) {
+	path := "testdata/config.yml"
+
+	cfg, _ := GetConfig(path)
+
+	if cfg.GetDatabasePort() != "3306" {
+		t.Errorf("got, want %s", cfg.GetDatabasePort())
+	}
+}
+
+func TestGetDatabaseName(t *testing.T) {
+	path := "testdata/config.yml"
+
+	cfg, _ := GetConfig(path)
+
+	if cfg.GetDatabaseName() != "food" {
+		t.Errorf("got, want %s", cfg.GetDatabaseName())
+	}
+}
+
+func TestGetDatabaseUser(t *testing.T) {
+	path := "testdata/config.yml"
+
+	cfg, _ := GetConfig(path)
+
+	if cfg.GetDatabaseUser() != "root" {
+		t.Errorf("got, want %s", cfg.GetDatabaseUser())
+	}
+}
+
+func TestGetDatabasePassword(t *testing.T) {
+	path := "testdata/config.yml"
+
+	cfg, _ := GetConfig(path)
+
+	if cfg.GetDatabasePassword() != "toor" {
+		t.Errorf("got, want %s", cfg.GetDatabasePassword())
+	}
+}
+
 func TestGetConfig(t *testing.T) {
 	senarios := []struct {
 		name     string
@@ -40,12 +110,5 @@ func TestGetConfig(t *testing.T) {
 				t.Errorf("%s: got, want %v", senario.name, senario.expected)
 			}
 		})
-	}
-
-	for _, senario := range senarios {
-		_, err := GetConfig(senario.path)
-		if err != nil && !senario.wantErr {
-			t.Errorf("%s: got, want %v", senario.name, senario.expected)
-		}
 	}
 }
